6bd95a279e68d3c2b00f98da16c58006c27e5ba0
md1300 <md.tawhid.khan1998@gmail.com>
2024-07-20 01:58:49 +0600

    blank commit

--- src/Register/Register.jsx
+++ src/Register/Register.jsx
@@ -1,14 +1,23 @@
 
+// import { useContext } from "react";
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { ContextProvider } from "../ContextApi/AuthProvider";
 
-
-
 const Register = () => {
-const context=useContext(ContextProvider)
+const {createUser}=useContext(ContextProvider)
+const handleRegisterForm=e=>{
+    e.preventDefault()
+    const name=e.target.name.value ;
+    const email=e.target.email.value ;
+    const password=e.target.password.value ;
+    const url=e.target.photoURL.value ;
 
-console.log(context)
+    createUser(email,password)
+
+    console.log(name,email,password,url,createUser)
+ 
+}
 
     return (
         <div className="hero bg-base-200 min-h-screen">
@@ -18,30 +27,30 @@ console.log(context)
            
           </div>
           <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-            <form className="card-body">
+            <form onSubmit={handleRegisterForm} className="card-body">
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Your name</span>
                 </label>
-                <input type="text" placeholder="your name" className="input input-bordered" required />
+                <input type="text" placeholder="your name" className="input input-bordered" name="name" required />
               </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
                 </label>
-                <input type="email" placeholder="email" className="input input-bordered" required />
+                <input type="email" placeholder="email" className="input input-bordered" name="email" required />
               </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Photo Url</span>
                 </label>
-                <input type="url" placeholder="photo url" className="input input-bordered" required />
+                <input type="url" placeholder="photo url" className="input input-bordered" name="photoURL" />
               </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Password</span>
                 </label>
-                <input type="password" placeholder="password" className="input input-bordered" required />
+                <input type="password" placeholder="password" className="input input-bordered" name="password" required />
                 <label className="label">
                   <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                 </label>
