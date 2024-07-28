74e4a2bf874279d570c30074ecc07c2dd2d7cf83
md1300 <md.tawhid.khan1998@gmail.com>
2024-07-28 13:34:56 +0600

    button inline

--- src/ShowEstateData/ShowEstateDate.jsx
+++ src/ShowEstateData/ShowEstateDate.jsx
@@ -4,23 +4,24 @@ const ShowEstateDate = ({showDatum}) => {
     const {image_url,estate_title,segment_name}=showDatum ;
     return (
         <div>
-            <div className="card card-compact bg-base-100 w-96 shadow-xl">
-  <figure>
-    <img     
-      src={image_url}
-      alt="Shoes"/>
-  </figure>
-  <div className="card-body">
-    <div>
+            <div className="card card-compact bg-base-100 w-96 shadow-xl ">
+      <figure >
+        <img
+            className="w-full h-48 object-cover"
+          src={image_url}
+          alt="Shoes"/>
+      </figure>
+      <div className="card-body">
+    <div className="w-full h-16 object-cover">
     <h2 className="card-title ">{estate_title}</h2>
     <p>{segment_name}</p>
     </div>
     
-    <div className="card-actions justify-end">
+    <div className="card-actions justify-end ">
       <button className="btn btn-primary">view details</button>
     </div>
-  </div>
-             </div>
+      </div>
+ </div>
         </div>
        
     );
