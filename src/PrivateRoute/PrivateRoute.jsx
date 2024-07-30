19769f671cc43a2c624a9095bd237dc58a79afb5
md1300 <md.tawhid.khan1998@gmail.com>
2024-07-30 06:27:53 +0600

    use navigate to go log in

--- src/PrivateRoute/PrivateRoute.jsx
+++ src/PrivateRoute/PrivateRoute.jsx
@@ -1,20 +1,24 @@
 import { useContext } from "react";
 import { ContextProvider } from "../ContextApi/AuthProvider";
-import { Navigate } from "react-router-dom";
+import {  Navigate, useLocation,  } from "react-router-dom";
+
 
 
 const PrivateRoute = ({children}) => {
     const {user,loading}=useContext(ContextProvider)
+    const location =useLocation()
+    console.log(location)
     
+
     if(loading){
         return <span className="loading loading-spinner loading-lg"></span>
     }
     if(user){
         return children
     }
-    else{
-     return <Navigate to="/logIn"></Navigate>
-    }
+    
+     return <Navigate to="/logIn"/>
+  
      
     
 };
